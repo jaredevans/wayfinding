d65d73072af8bf256e3979ac227c926bf032eca9
jared <jnevans@gmail.com>
2025-07-30 10:08:02 -0400

    adding activate_venv.sh

---
+++ activate_venv.sh
@@ -0,0 +1,15 @@
+# cat activate_venv.sh
+#!/bin/bash
+
+# AlERT: source this script, don't run it directly.
+# source activate_venv.sh
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  echo "This script must be sourced, not run directly."
+  echo "source activate_venv.sh"
+  exit 1
+fi
+
+# rest of your script here
+echo "Script is being sourced. Continuing..."
+source ./.venv/bin/activate
